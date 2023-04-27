77ccf8e21c7e58cefbd1dbe0b6bc60ef96f33295
selmaAlzarrouk <selma.alzarrouk@hotmail.com>
2023-04-27 21:05:51 +0100

    uPDATE Contact screen . js

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -2,80 +2,136 @@ import React, { Component } from "react";
 import { ActivityIndicator, View, StyleSheet, Text, TextInput, TouchableOpacity, Button } from "react-native";
 import { FlatList } from "react-native-web";
 import ContactList from "../components/ContactList";
+//import { Settings } from '@material-ui/icons';
 
 import { getContactList } from "../api/apiController";
 import { render } from "react-dom";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+
+
 export default class ContactsScreen extends Component {
+
     constructor(props) {
         super(props);
-
-
-        this.state = {
+      /* */  this.state = {
             // this boolean is Trrue becuase I set
             //contact data has been fetched or not
             //its the checker for the api 
             isLoading: true,
             contactData: [],  //this holds the contact data
             query: ``,
-            userList: []
-
+            userList: [],
+            message: '',
+            user_id : 0
         };
     }
-
     componentDidMount() {
+        this.unsubscribe = this.props.navigation.addListener('focus', ()=>{
+            console.log("Screen reached");
+            this.getData();
+            // below I want to grab the Contact List data so : 
+            getContactList()
+                .then((responseJson) => {
+                    this.setState({
+                        isLoading: false,
+                        contactData: responseJson
+                    });
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        })
         //called when the screen is focused, it is called once  
         //when the screen is focused by the user
-        console.log("Screen reached");
-        this.getData();
-        // below I want to grab the Contact List data so : 
-        getContactList()
-            .then((responseJson) => {
-                this.setState({
-                    isLoading: false,
-                    contactData: responseJson
-                });
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+     
     }
     componentWillUnmount() {
         //called when screen is unfocused
         this.unsubscribe();
     }
 
-//Will add a user to the logged in users list of contacts
+
+
+
+/**
+ * 
+ * @returns 
+ */
+/* I AM HERE : 
+    //Will add a user to the logged in users list of contacts
+    //  basically pass in an argument into the this.addContact function
     async addContact() { //perfromes fetch req for users
-        return fetch(`http://localhost:3333/api/1.0.0/user/{user_id}/contact`, {
-            method: `POST`,
+        return fetch('http://localhost:3333/api/1.0.0/user/{user_id}/contact', {
+            method: 'post',
             headers: {
                 'x-authorization': await AsyncStorage.getItem('whatsthat_session_token'),
             },
         })
             .then((response) => response.json())
             .then((responseJson) => {
-                this.setState({
-                    isLoading: false,
-                    userList: responseJson,
-
-                });
+                // code to add the contact
+                this.setState({ message: 'Added to Contacts' })
+                
             })
             .catch((error) => {
                 console.log(error);
 
             })
     }
+    */
+    async addContact() {
+        
+        try {
+            const token = await AsyncStorage.getItem('whatsthat_session_token');
+            const response = await fetch('http://localhost:3333/api/1.0.0/user/'+this.state.user_id+'/contact', {
+                method: 'post',
+                headers: {
+                    'x-authorization': token,
+                },
+            });
+    
+            if (response.status === 200) {
+                const responseJson = await response.json();
+                // code to add the contact
+                this.setState({ message: 'Added to Contacts' })
+            } else if (response.status === 400) {
+                throw 'You can\'t add yourself as a contact';
+            } else if (response.status === 401) {
+                throw 'Unauthorized';
+            } else if (response.status === 404) {
+                throw 'Not Found';
+            } else if (response.status === 500) {
+                throw 'Server Error';
+            } else {
+                throw 'Something went wrong';
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+    addingContactHandler = (id) => {
+        this.setState({ user_id: id }, () => {
+            this.addContact();
+        });
+    };
     // this will deal with the search of users 
     async getData() { //perfromes fetch req for users
+        //The search function sends a fetch request to a local server at URL endpoint
+        //passing the search query as a parameter in the URL :>
         return fetch(`http://localhost:3333/api/1.0.0/search?q=${this.state.query}`, {
             method: `GET`,
             headers: {
+                //request is authenticated with a session token retrieved from the device's AsyncStorage. 
+
                 'x-authorization': await AsyncStorage.getItem('whatsthat_session_token'),
             },
         })
             .then((response) => response.json())
+            //The response is expected to be in JSON format and is stored in the component's state 
+            //under the userList key.
             .then((responseJson) => {
                 this.setState({
                     isLoading: false,
@@ -88,16 +144,22 @@ export default class ContactsScreen extends Component {
 
             })
     }
+
+    //queryHandler function updates the state with latest search query entered in the text input field,
+    //then the searchUsers function calls the getData function to fetch the results.
     queryHandler = (searchQuery) => {
         this.setState({ query: searchQuery })
         console.log(this.state.query
         );
     }
-
-    searchUsers = () =>{
+    searchUsers = () => {
         this.getData();
     };
-
+    //the component renders a FlatList to display the list of users returned by the search query.
+    // The FlatList component receives the userList array as data and iterates through it to render each user's details.
+    // Each user is rendered as a View containing their given name, family name, and email.
+    // The keyExtractor function extracts the unique id field 
+    //from each user object or uses the index as a fallback if the id field is not available.
     render() {
         return (
             //everything inside here
@@ -108,18 +170,26 @@ export default class ContactsScreen extends Component {
                     onChangeText={this.queryHandler}
                 />
                 <Button title="Search" onPress={this.searchUsers} />
-                <FlatList
+                <FlatList   
                     data={this.state.userList}
                     renderItem={({ item }) => (
                         <View>
-                            <Text>{item.given_name}{item.family_name}{item.email}</Text>
+                            <Text>{item.given_name}  {item.family_name}</Text>
+                            <Text>{item.email}</Text>
+
+                            <TouchableOpacity onPress={() => this.addingContactHandler(item.user_id)}>
+                            <Text>Add  to Contact</Text>
+                            </TouchableOpacity>
+                            <Text>{this.state.message}</Text>
                         </View>
                     )}
                     keyExtractor={({ id }, index) => id ? id.toString() : index.toString()}
                 />
+
             </View>
 
         )
 
     }
 }
+
