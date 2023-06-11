3ebbd13e5b7d7438a7c2bd8699977d5e27ea8e45
selmaAlzarrouk <80717479+selmaAlzarrouk@users.noreply.github.com>
2023-06-11 21:19:50 +0100

    implemented buttons for chat management screen , rather than just have it show delete contact or add contact to the group chat
    react native ele icons

--- screens/chatGroupManagement.js
+++ screens/chatGroupManagement.js
@@ -1,17 +1,47 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/no-unused-state */
 import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
 import {
-  View, Text, TouchableOpacity,
+  View, Text, TouchableOpacity,ScrollView,
 } from 'react-native';
 import { FlatList } from 'react-native-web';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { ListItem } from 'react-native-elements';
+import { Icon,ListItem} from 'react-native-elements';
+
 
 import {
   addUsertoChat, getContactLisData, getSingleChat, deleteUserinChat,
 } from '../api/apiController';
 
+const styles = StyleSheet.create({
+  contain: {
+    flex: 1,
+    paddingHorizontal: 10,
+    paddingTop: 10,
+  },
+  section: {
+    marginBottom: 20,
+  },
+  subheading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  actContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    marginRight: 5,
+  },
+  deleteTxt: {
+    color: 'red',
+  },
+  addText: {
+    color: 'green',
+  },
+});
 export default class chatGroupManagement extends Component {
   constructor(props) {
     super(props);
@@ -67,48 +97,64 @@ export default class chatGroupManagement extends Component {
 
   render() {
     return (
-      <View>
-        <Text>{this.state.error}</Text>
-        <FlatList
-          data={this.state.chatData.members}
-          renderItem={({ item }) => (
-            <ListItem
-              onPress={() => this.deleteContactHandler(item.user_id)}
-              bottomDivider
-            >
-              <ListItem.Content>
-                <ListItem.Title>{`${item.first_name} ${item.last_name}`}</ListItem.Title>
-                <ListItem.Subtitle>{item.email}</ListItem.Subtitle>
-                <TouchableOpacity onPress={() => this.deletUser(item.user_id)}>
-                  <Text style={{ color: 'red' }}>Delete Contact</Text>
-                </TouchableOpacity>
-              </ListItem.Content>
-              <ListItem.Chevron />
-            </ListItem>
-          )}
-          keyExtractor={({ id }, index) => (id ? id.toString() : index.toString())}
-        />
-        <Text>Friend List</Text>
-        <FlatList
-          data={this.state.contactData}
-          renderItem={({ item }) => (
-            <ListItem
-              onPress={() => this.deleteContactHandler(item.user_id)}
-              bottomDivider
-            >
-              <ListItem.Content>
-                <ListItem.Title>{`${item.first_name} ${item.last_name}`}</ListItem.Title>
-                <ListItem.Subtitle>{item.email}</ListItem.Subtitle>
-                <TouchableOpacity onPress={() => this.addUser(item.user_id)}>
-                  <Text style={{ color: 'green' }}>Add Contact</Text>
-                </TouchableOpacity>
-              </ListItem.Content>
-              <ListItem.Chevron />
-            </ListItem>
-          )}
-          keyExtractor={({ id }, index) => (id ? id.toString() : index.toString())}
-        />
-      </View>
-    );
-  }
+        <ScrollView>
+          <View style={styles.contain}>
+            <Text>{this.state.error}</Text>
+            <View style={styles.section}>
+              <Text style={styles.subheading}>Members In Chats</Text>
+              <FlatList
+                data={this.state.chatData.members}
+                renderItem={({ item }) => (
+                  <ListItem
+                    onPress={() => this.deleteContactHandler(item.user_id)}
+                    bottomDivider
+                  >
+                    <ListItem.Content>
+                      <ListItem.Title>{`${item.first_name} ${item.last_name}`}</ListItem.Title>
+                      <ListItem.Subtitle>{item.email}</ListItem.Subtitle>
+      
+                      <TouchableOpacity onPress={() => this.deleteUser(item.user_id)} style={styles.actContainer}>
+                        <Icon name="trash" type="font-awesome" color="red" style={styles.icon} />
+                        <Text style={styles.deleteTxt}>Delete Contact</Text>
+                      </TouchableOpacity> 
+      
+                    </ListItem.Content>
+                    <ListItem.Chevron />
+                  </ListItem>
+                )}
+                keyExtractor={({ id }, index) => (id ? id.toString() : index.toString())}
+              />
+            </View>
+            <View style={styles.section}>
+              <Text style={styles.subheading}>Friend List</Text>
+              <FlatList
+                data={this.state.contactData}
+                renderItem={({ item }) => (
+                  <ListItem
+                    onPress={() => this.deleteContactHandler(item.user_id)}
+                    bottomDivider
+                  >
+                    <ListItem.Content>
+                      <ListItem.Title>{`${item.first_name} ${item.last_name}`}</ListItem.Title>
+                      <ListItem.Subtitle>{item.email}</ListItem.Subtitle>
+      
+                      <TouchableOpacity onPress={() => this.addUser(item.user_id)} style={styles.actContainer}>
+                        <Icon name="user-plus" type="font-awesome" color="green" style={styles.icon} />
+                        <Text style={styles.addText}>Add Contact</Text>
+                      </TouchableOpacity> 
+      
+                    </ListItem.Content>
+                    <ListItem.Chevron />
+                  </ListItem>
+                )}
+                keyExtractor={({ id }, index) => (id ? id.toString() : index.toString())}
+              />
+            </View>
+          </View>
+        </ScrollView>
+      );
+      
+    }
 }
+
+
---
+++ styles/globalStyles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+
+const backbtnStyle = StyleSheet.create({
+    backbtnStyle: {
+        marginLeft: 12,
+        width: '50%',
+    }
+})
+
+export default backbtnStyle;
