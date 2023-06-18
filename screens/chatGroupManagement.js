07e09b74038ad06f9f8cdff4776f79008d8012ec
selmaAlzarrouk <80717479+selmaAlzarrouk@users.noreply.github.com>
2023-06-18 23:27:48 +0100

    A11Y labels accesiblithy true and implemeted dark mode and light mode ensuring users can access the app in different colour theme!s
    A11Y - accesibility and theme provision! : )

--- screens/chatGroupManagement.js
+++ screens/chatGroupManagement.js
@@ -1,19 +1,25 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/no-unused-state */
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   StyleSheet,
-  View, Text, TouchableOpacity, ScrollView,
-} from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { FlatList } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Icon, ListItem } from 'react-native-elements';
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { FlatList } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Icon, ListItem, ThemeProvider, Switch } from "react-native-elements";
 
 import {
-  addUsertoChat, getContactLisData, getSingleChat, deleteUserinChat,
-} from '../api/apiController';
+  addUsertoChat,
+  getContactLisData,
+  getSingleChat,
+  deleteUserinChat,
+} from "../api/apiController";
 
 const styles = StyleSheet.create({
   contain: {
@@ -26,33 +32,33 @@ const styles = StyleSheet.create({
   },
   subheading: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
   },
   actContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   icon: {
     marginRight: 5,
   },
   deleteTxt: {
-    color: 'red',
+    color: "red",
   },
   addText: {
-    color: 'green',
+    color: "green",
   },
 });
-export default class chatGroupManagement extends Component {
+
+export default class ChatGroupManagement extends Component {
   constructor(props) {
     super(props);
     this.state = {
       chatData: [],
       contactData: [],
-      error: '',
+      error: "",
+      theme: "light",
     };
-
-    this.deleteUser = this.deleteUser.bind(this);
   }
 
   componentDidMount() {
@@ -61,107 +67,203 @@ export default class chatGroupManagement extends Component {
   }
 
   async getChatData() {
-    getSingleChat(
-      await AsyncStorage.getItem('chatID'),
-      ((response) => { this.setState({ chatData: response }); }),
-    );
+    getSingleChat(await AsyncStorage.getItem("chatID"), (response) => {
+      this.setState({ chatData: response });
+    });
   }
 
   async getContactData() {
     getContactLisData(
-      ((response) => { this.setState({ contactData: response }); }),
-      ((err) => { this.setState({ error: err }); }),
+      (response) => {
+        this.setState({ contactData: response });
+      },
+      (err) => {
+        this.setState({ error: err });
+      }
     );
   }
 
   addUser = async (userId) => {
     addUsertoChat(
-      await AsyncStorage.getItem('chatID'),
-      userId, (
-        () => {
-          this.getChatData();
-          this.getContactData();
-        }),
-      (() => {}),
+      await AsyncStorage.getItem("chatID"),
+      userId,
+      () => {
+        this.getChatData();
+        this.getContactData();
+      },
+      () => {}
     );
   };
 
   deleteUser = async (userId) => {
     deleteUserinChat(
-      await AsyncStorage.getItem('chatID'),
-      userId, (
-        () => {
-          this.getChatData();
-          this.getContactData();
-        }),
-      (() => {}),
+      await AsyncStorage.getItem("chatID"),
+      userId,
+      () => {
+        this.getChatData();
+        this.getContactData();
+      },
+      () => {}
     );
   };
 
+  toggleTheme = () => {
+    this.setState((prevState) => ({
+      theme: prevState.theme === "light" ? "dark" : "light",
+    }));
+  };
+
   render() {
+    const { theme } = this.state;
+    const bgColour = theme === "dark" ? "#000000" : "#ffffff";
+    const textColour = theme === "dark" ? "#ffffff" : "#000000";
+
     return (
-      <ScrollView>
-        <View style={styles.contain}>
-          <TouchableOpacity onPress={() => this.props.navigation.goBack(null)}>
-            <Ionicons name="arrow-back" size="large" />
-          </TouchableOpacity>
-          <Text>{this.state.error}</Text>
-          <View style={styles.section}>
-            <Text style={styles.subheading}>Members In Chats</Text>
-            <FlatList
-              data={this.state.chatData.members}
-              renderItem={({ item }) => (
-                <ListItem
-                  bottomDivider
-                >
-                  <ListItem.Content>
-                    <ListItem.Title>{`${item.first_name} ${item.last_name}`}</ListItem.Title>
-                    <ListItem.Subtitle>{item.email}</ListItem.Subtitle>
+      <ThemeProvider useDark={theme === "dark"}>
+        <ScrollView>
+          <View
+            style={[styles.contain, { backgroundColor: bgColour }]}
+            accessible={true}
+            accessibilityLabel="Chat Group Management"
+          >
+            <TouchableOpacity
+              onPress={() => this.props.navigation.goBack(null)}
+              accessible={true}
+              accessibilityLabel="Go Back"
+            >
+              <Ionicons
+                name="arrow-back"
+                size={24}
+                accessible={true}
+                accessibilityLabel="Back Icon"
+              />
+            </TouchableOpacity>
+            <Text
+              style={{ color: textColour }}
+              accessible={true}
+              accessibilityLabel="Error Message"
+            >
+              {this.state.error}
+            </Text>
+            <View style={styles.section}>
+              <Text
+                style={[styles.subheading, { color: textColour }]}
+                accessible={true}
+                accessibilityLabel="Members In Chats"
+              >
+                Members In Chats
+              </Text>
+              <FlatList
+                data={this.state.chatData.members}
+                renderItem={({ item }) => (
+                  <ListItem
+                    bottomDivider
+                    accessible={true}
+                    accessibilityRole="listitem"
+                  >
+                    <ListItem.Content>
+                      <ListItem.Title
+                        style={{ color: textColour }}
+                        accessible={true}
+                        accessibilityLabel={`${item.first_name} ${item.last_name}`}
+                      >{`${item.first_name} ${item.last_name}`}</ListItem.Title>
+                      <ListItem.Subtitle
+                        style={{ color: textColour }}
+                        accessible={true}
+                        accessibilityLabel={item.email}
+                      >
+                        {item.email}
+                      </ListItem.Subtitle>
 
-                    <TouchableOpacity
-                      onPress={() => this.deleteUser(item.user_id)}
-                      style={styles.actContainer}
-                    >
-                      <Icon name="trash" type="font-awesome" color="red" style={styles.icon} />
-                      <Text style={styles.deleteTxt}>Delete Contact</Text>
-                    </TouchableOpacity>
+                      <TouchableOpacity
+                        onPress={() => this.deleteUser(item.user_id)}
+                        style={styles.actContainer}
+                        accessible={true}
+                        accessibilityLabel={`Delete Contact for ${item.first_name}`}
+                      >
+                        <Icon
+                          name="trash"
+                          type="font-awesome"
+                          color="red"
+                          style={styles.icon}
+                        />
+                        <Text style={[styles.deleteTxt, { color: textColour }]}>
+                          Delete Contact
+                        </Text>
+                      </TouchableOpacity>
+                    </ListItem.Content>
+                    <ListItem.Chevron />
+                  </ListItem>
+                )}
+                keyExtractor={({ id }, index) =>
+                  id ? id.toString() : index.toString()
+                }
+              />
+            </View>
+            <View style={styles.section}>
+              <Text
+                style={[styles.subheading, { color: textColour }]}
+                accessible={true}
+                accessibilityLabel="Friend List"
+              >
+                Friend List
+              </Text>
+              <FlatList
+                data={this.state.contactData}
+                renderItem={({ item }) => (
+                  <ListItem
+                    bottomDivider
+                    accessible={true}
+                    accessibilityRole="listitem"
+                  >
+                    <ListItem.Content>
+                      <ListItem.Title
+                        style={{ color: textColour }}
+                        accessible={true}
+                        accessibilityLabel={`${item.first_name} ${item.last_name}`}
+                      >{`${item.first_name} ${item.last_name}`}</ListItem.Title>
+                      <ListItem.Subtitle
+                        style={{ color: textColour }}
+                        accessible={true}
+                        accessibilityLabel={item.email}
+                      >
+                        {item.email}
+                      </ListItem.Subtitle>
 
-                  </ListItem.Content>
-                  <ListItem.Chevron />
-                </ListItem>
-              )}
-              keyExtractor={({ id }, index) => (id ? id.toString() : index.toString())}
-            />
+                      <TouchableOpacity
+                        onPress={() => this.addUser(item.user_id)}
+                        style={styles.actContainer}
+                        accessible={true}
+                        accessibilityLabel={`Add Contact for ${item.first_name}`}
+                      >
+                        <Icon
+                          name="user-plus"
+                          type="font-awesome"
+                          color="green"
+                          style={styles.icon}
+                        />
+                        <Text style={[styles.addText, { color: textColour }]}>
+                          Add Contact
+                        </Text>
+                      </TouchableOpacity>
+                    </ListItem.Content>
+                    <ListItem.Chevron />
+                  </ListItem>
+                )}
+                keyExtractor={({ id }, index) =>
+                  id ? id.toString() : index.toString()
+                }
+              />
+            </View>
           </View>
-          <View style={styles.section}>
-            <Text style={styles.subheading}>Friend List</Text>
-            <FlatList
-              data={this.state.contactData}
-              renderItem={({ item }) => (
-                <ListItem
-                  bottomDivider
-                >
-                  <ListItem.Content>
-                    <ListItem.Title>{`${item.first_name} ${item.last_name}`}</ListItem.Title>
-                    <ListItem.Subtitle>{item.email}</ListItem.Subtitle>
-
-                    <TouchableOpacity
-                      onPress={() => this.addUser(item.user_id)}
-                      style={styles.actContainer}
-                    >
-                      <Icon name="user-plus" type="font-awesome" color="green" style={styles.icon} />
-                      <Text style={styles.addText}>Add Contact</Text>
-                    </TouchableOpacity>
-
-                  </ListItem.Content>
-                  <ListItem.Chevron />
-                </ListItem>
-              )}
-              keyExtractor={({ id }, index) => (id ? id.toString() : index.toString())}
-            />
-          </View>
-        </View>
-      </ScrollView>
+          <Switch
+            value={theme === "dark"}
+            onValueChange={this.toggleTheme}
+            accessible={true}
+            accessibilityLabel="Toggle Dark Mode"
+          />
+        </ScrollView>
+      </ThemeProvider>
     );
   }
 }
